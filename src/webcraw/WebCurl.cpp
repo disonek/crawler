f4e3f38a6da5ade3774691669fbbf05809137178
disonek <disonek@gmail.com>
2020-11-02 23:17:12 +0100

    good change to cpr

--- src/webcraw/WebCurl.cpp
+++ src/webcraw/WebCurl.cpp
@@ -4,35 +4,17 @@
 
 #include <stdexcept>
 #include <string>
+#include <cpr/cpr.h>
 
-size_t WebCurl::writeCallback(char* buf, size_t size, size_t nmemb, void* up)
-{
-    // callback must have this declaration
-    // buf is a pointer to the data that curl has for us
-    // size*nmemb is the size of the buffer
-    ((std::string*)up)->append((char*)buf, size * nmemb);
-    return size * nmemb; // tell curl how many bytes we handled
-}
 
 std::string WebCurl::getPage(const std::string& url)
 {
-    CURLcode code;
-    CURL* curl;
-    curl_global_init(CURL_GLOBAL_DEFAULT);
-    std::string readBuffer;
-    curl = curl_easy_init();
-    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
-    if(curl == nullptr)
-        throw std::runtime_error("Failed to create CURL connection");
-    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-    // if (code != CURLE_OK)
-    //         throw std::runtime_error("There is something wrong with your URL");
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &WebCurl::writeCallback);
-    // curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L); //tell curl to output its progress
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
-    code = curl_easy_perform(curl);
-    curl_easy_cleanup(curl);
-    if(code != CURLE_OK)
-        throw std::runtime_error("Failed to retrieve web page");
-    return readBuffer;
+    cpr::Response r = cpr::Get(cpr::Url{url}, 
+                    cpr::VerifySsl(false),
+                    cpr::Parameters{{"anon", "true"}, {"key", "value"}});
+                      
+    r.status_code;                  // 200
+    r.header["content-type"];       // application/json; charset=utf-8
+    r.text;                         // JSON text string
+    return r.text;
 }
--- src/webcraw/WebCurl.hpp
+++ src/webcraw/WebCurl.hpp
@@ -4,8 +4,6 @@
 #include <string>
 
 class WebCurl {
-private:
-    static size_t writeCallback(char *buf, size_t size, size_t nmemb, void *up);
 public:
     static std::string getPage(const std::string &url);
 };
